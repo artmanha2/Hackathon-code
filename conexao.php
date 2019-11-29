3836fa711980ab28d93f0e7abf446d3259135bd3
Tisgo Missio <50518467+artmanha2@users.noreply.github.com>
2019-11-29 20:02:09 -0300

    Add files via upload

---
+++ conexao.php
@@ -0,0 +1,10 @@
+<?php
+    $servidor =  "localhost";
+    $usuario = "root";
+    $senha = "";
+    $dbname = "appbakof";
+
+    //conexão
+
+    $conn = mysqli_connect($servidor, $usuario, $senha, $dbname);
+        
