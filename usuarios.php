b299e32ab935bc8623e76ece6c665ffdd3cf200e
vjssjv15 <58346923+vjssjv15@users.noreply.github.com>
2019-11-30 01:21:09 -0300

    Add files via upload

---
+++ usuarios.php
@@ -0,0 +1,28 @@
+<?php
+
+?>
+<!DOCTYPE html>
+<html lang ="pt-br">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport", content="width=device-width, initial-scale=1.0" />
+        <link href= "css/bootstrap.min.css" rel="stylesheet" type="text/css"/>
+        <script href= "text/javascript" src="js/bootstrap.min.js" ></script>
+
+        <title>BAKOFTEC - Transporte</title>
+
+    </head>
+    
+    <body>
+        
+        <header>
+           <div class="p-3 mb-2 bg-secondary text-white">
+                <center><h5>BAKOFTEC TRANSPORTES</h5></center>
+            </div>
+        </header>
+        <h1>BAKOFTEC</h1>
+        <button type="button"> Heloo</button> 
+    </body>
+
+
+</html>
