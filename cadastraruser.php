c948db746cfc4c93b7a0c3a6ba138d34af86a38c
vjssjv15 <58346923+vjssjv15@users.noreply.github.com>
2019-11-29 22:55:55 -0300

    Add files via upload

--- cadastraruser.php
+++ cadastraruser.php
@@ -5,38 +5,32 @@
         <meta name="viewport", content="width=device-width, initial-scale=1.0" />
         <link href= "css/bootstrap.min.css" rel="stylesheet" type="text/css"/>
         <script href= "text/javascript" src="js/bootstrap.min.js" ></script>
-        <title>BAKOFTEC</title>
+        <title>BAKOFTEC - Cadastros</title>
     
     </head>
-
     <body>
-        <header>
+         <header>
             <div class="p-3 mb-2 bg-secondary text-white">
-                <center><h3>BAKOFTEC -> SISTEMA PARA TRANSPORTES</h3></center>
+                <center><h3>BAKOFTEC -> CADASTROS</h3></center>
             </div>
         </header>
         
         <center>
+            <h3><p class="text-success">Insira as informações do motorista!</p></h3>
+            
+            <h5>Nome</h5><input type="text" name="nome" placeholder="Nome completo"><br>
+            
+            <h5>E-mail</h5><input type="email" name="email" placeholder="E-mail"><br>
+            
+            <h5>Celular</h5><input type="tel" name="celular" placeholder="Telefone / Celular"><br>
             
             <h5>CPF</h5><input type="number" name="cpf" placeholder="CPF "><br>
             
             <h5>Senha</h5><input type="password" name="senha" placeholder="Senha"><br>
-        
+            
         </center>
         
-        
+    
     </body>
-
-
-
-
-
-
-
-
-
-
-
-
 
 </html>
--- principal.php
+++ principal.php
@@ -1,42 +1,22 @@
 <!DOCTYPE html>
-<html lang="pt-br">
+<html lang ="pt-br">
     <head>
         <meta charset="utf-8">
         <meta name="viewport", content="width=device-width, initial-scale=1.0" />
         <link href= "css/bootstrap.min.css" rel="stylesheet" type="text/css"/>
         <script href= "text/javascript" src="js/bootstrap.min.js" ></script>
-        <title>BAKOFTEC</title>
-    
+        <title>BAKOFTEC - Transporte</title>
     </head>
-
+    
     <body>
         <header>
             <div class="p-3 mb-2 bg-secondary text-white">
-                <center><h3>BAKOFTEC -> SISTEMA PARA TRANSPORTES</h3></center>
+                <center><h3>Transportes</h3></center>
             </div>
         </header>
-        
-        <center>
-            
-            <h5>CPF</h5><input type="number" name="cpf" placeholder="CPF "><br>
-            
-            <h5>Senha</h5><input type="password" name="senha" placeholder="Senha"><br>
-        
-        </center>
-        
-        
+        <h1>BAKOFTEC</h1>
+        <i class="fas fa-home"></i>">
     </body>
-
-
-
-
-
-
-
-
-
-
-
 
 
 </html>
